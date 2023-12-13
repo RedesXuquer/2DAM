57d04f09f6d909a292cbe99f39f6552063ff3ae5
laleza <laleza@axuquer.es>
2023-12-13 08:40:50 +0100

    Añado un nuevo proyecto en Hibernate hecho en clase

---
+++ Hibernate/ClaseHibernate/src/clasehibernate/FXMLDocumentController.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
+ */
+package clasehibernate;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javax.sound.midi.ShortMessage;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ *
+ * @author denos
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        configuration.addAnnotatedClass(Song.class);
+        
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        
+        List<Song> artistas = session.createQuery("from Song", Song.class).getResultList();
+        
+        for (Song artista: artistas){
+            System.out.println(artista.getArtist());
+        }
+        
+        session.getTransaction().commit();
+            // Cerrar la sesión de Hibernate
+            sessionFactory.close();
+        
+        
+    }    
+    
+}
